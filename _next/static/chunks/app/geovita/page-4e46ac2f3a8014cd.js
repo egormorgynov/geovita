0500ac5fba39423def25c60bfb87537861d2a2e2
egormorgunov <egormorgunov412@gmail.com>
2026-08-13 20:37:13 +0300

    Implement multi-role GeoVita workflow

---
+++ _next/static/chunks/app/geovita/page-4e46ac2f3a8014cd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[926],{8832:(e,s,_)=>{Promise.resolve().then(_.bind(_,929))}},e=>{e.O(0,[740,836,929,120,208,358],()=>e(e.s=8832)),_N_E=e.O()}]);
